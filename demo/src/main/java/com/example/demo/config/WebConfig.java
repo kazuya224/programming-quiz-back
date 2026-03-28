042f62f4063375e22dbd270cd16b146b741c2c7a
Kazuya Ogawa <jiashouyexiaochuan@gmail.com>
2026-03-28 18:26:02 +0900

    CORS再設定

--- demo/src/main/java/com/example/demo/config/WebConfig.java
+++ demo/src/main/java/com/example/demo/config/WebConfig.java
@@ -1,20 +1,20 @@
-// src/main/java/com/example/demo/config/WebConfig.java
-package com.example.demo.config;
+// // src/main/java/com/example/demo/config/WebConfig.java
+// package com.example.demo.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // ←全部対象にする
-                .allowedOrigins(
-                        "http://localhost:3000",
-                        "https://programing-quiz-zeta.vercel.app")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
-}
+// @Configuration
+// public class WebConfig implements WebMvcConfigurer {
+// @Override
+// public void addCorsMappings(CorsRegistry registry) {
+// registry.addMapping("/**") // ←全部対象にする
+// .allowedOrigins(
+// "http://localhost:3000",
+// "https://programing-quiz-zeta.vercel.app")
+// .allowedMethods("*")
+// .allowedHeaders("*")
+// .allowCredentials(true);
+// }
+// }
