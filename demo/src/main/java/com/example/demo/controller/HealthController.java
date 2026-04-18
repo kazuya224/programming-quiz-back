85905da75516542794237e46e07e0230bb779cae
Kazuya Ogawa <jiashouyexiaochuan@gmail.com>
2026-04-18 23:20:58 +0900

    API追加

---
+++ demo/src/main/java/com/example/demo/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public String health() {
+        return "ok";
+    }
+
+}
